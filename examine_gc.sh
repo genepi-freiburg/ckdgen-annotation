d848c5cbe2961763fb399ad0266c78c9186ba0a4
Matthias Wuttke <matthias@wuttke.eu>
2017-06-12 12:46:29 +0200

    Furthers cripts

---
+++ examine_gc.sh
@@ -0,0 +1,1 @@
+grep "Genomic control" metal.log
---
+++ get_top_results.sh
@@ -0,0 +1,32 @@
+INFN=$1
+OUTFN=$2
+TOPN=$3
+
+if [ ! -f "$INFN" ]
+then
+	echo "Input file does not exist: $INFN"
+	exit 3
+fi
+
+if [ -f "$OUTFN" ]
+then
+	echo "Output file exists - please remove: $OUTFN"
+	exit 3
+fi
+
+if [ "$TOPN" == "" ]
+then
+	TOPN=10000
+	echo "Use default for TopN parameter: 10000"
+fi
+
+echo "Retrieve top $TOPN rows"
+echo "Input file: $INFN"
+echo "Output file: $OUTFN"
+
+# keep header
+head -n 1 $INFN > $OUTFN
+
+# sort -g can sort exponential numbers
+sort -g -k 10 $INFN | head -n $TOPN >> $OUTFN
+
