f15f920143d8df70fb493bca588ffde34094cc0a
Matthias Wuttke <matthias@wuttke.eu>
2017-06-13 08:51:21 +0200

    Bugfixes Annotate-Script

--- do_annotate.sh
+++ do_annotate.sh
@@ -1,6 +1,6 @@
 INFN=$1
 OUTFN=$2
-ANNOTATE_FILE=/shared/annotation/data/SNP_HG19_Masterfile-170607.txt.gz
+ANNOTATE_FILE=/shared/annotation/data/tabix/SNP_HG19_Masterfile-170607.txt.gz
 
 if [ ! -f "$INFN" ]
 then
@@ -19,7 +19,13 @@ echo "Output file: $OUTFN"
 echo "Annotation file: $ANNOTATE_FILE"
 
 # Process Substitution - unzip on-the-fly before joining
-join $INFN <(zcat $ANNOTATE_FILE) -1 1 -2 3 > $OUTFN
+INFN_HEADER=`head -n 1 $INFN`
+ANNO_HEADER=`zcat $ANNOTATE_FILE | head -n 1`
+ANNO_HEADER2=`echo $ANNO_HEADER | sed 's/MARKER //' | sed 's/ /	/g'`
+echo "$INFN_HEADER	$ANNO_HEADER2" > $OUTFN
+
+# file 1: col 1, keep unpairable lines; file 2: col 3
+join <(tail -n+2 $INFN) <(zcat $ANNOTATE_FILE | tail -n+2) -1 1 -2 3 -a 1 -t'	' >> $OUTFN
 
 wc -l $INFN $OUTFN
 
