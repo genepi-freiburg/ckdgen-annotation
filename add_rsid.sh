c23209a264b69d5236359cc62194645078b92517
Matthias Wuttke <matthias@wuttke.eu>
2017-09-20 21:59:47 +0200

    Keep unpairable lines during join

--- add_rsid.sh
+++ add_rsid.sh
@@ -1,6 +1,6 @@
 INFN=$1
 OUTFN=$2
-ANNOTATE_FILE=/shared/annotation/data/SNP_Jointable.txt.gz
+ANNOTATE_FILE=/shared/annotation/data/SNP_Jointable.unique.nonempty.txt.gz
 
 if [ ! -f "$INFN" ]
 then
@@ -36,10 +36,30 @@ echo "$INFN_HEADER	$ANNO_HEADER2" > $OUTFN
 # Process Substitution - unzip on-the-fly before joining
 # file 1: col 1, keep unpairable lines; file 2: col 1
 echo "Join files"
-join <(cat ${INFN} | tail -n+2 | sort -k1) <(zcat $ANNOTATE_FILE | tail -n+2) -1 1 -2 1 -t'	' >> $OUTFN
 
-#echo "Sort $OUTFN"
-#(head -n 1 $OUTFN && tail -n +2 $OUTFN | sort -k1,2.1) > ${OUTFN}.sorted
-#mv ${OUTFN}.sorted $OUTFN
+KEEP=1
+
+if [ "$KEEP" == "1" ]
+then
+	KEEP_OPT="-a 1"
+	echo "Keep lines without RSID"
+else
+	# don't keep lines with missing RSID
+	KEEP_OPT=""
+	echo "Drop lines without RSID"
+fi
+
+join <(cat ${INFN} | tail -n+2 | sort -k1) <(zcat $ANNOTATE_FILE | tail -n+2) -1 1 -2 1 -t'	' $KEEP_OPT >> $OUTFN
+
+wc -l $OUTFN ${INFN}
+
+
+if [ "$KEEP" == "1" ]
+then
+	# TODO determine col numbers dynamically
+	echo "Dump missing RSIDs to ${OUTFN}.missing"
+	cat $OUTFN | awk '{ if ($20 == "" || FNR==1) { print $1,$2, $3,$20 }}' > ${OUTFN}.missing
+	wc -l ${OUTFN}.missing
+fi
 
 echo "Done"
